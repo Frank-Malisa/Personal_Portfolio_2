b539089a42a4e945891129f9942ee0532c280083
Frank-Malisa <fmalisah02@gmail.com>
2022-08-27 22:25:22 +0300

    update

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ window.onscroll = () =>
     document.querySelectorAll('section').forEach(sec => {
 
         let top = window.scrollY;
-        let offset = sec.offsetTop;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -27,7 +27,7 @@ window.onscroll = () =>
         {
             document.querySelectorAll('.header .navbar a').forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('.header .navbar a[href*='+ id +']').classList.add('active')
+                document.querySelector('.header .navbar a[href*='+ id +']').classList.add('active');
             });
         };
 
